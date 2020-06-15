1157fa72b9366c680d049715dedf99b178b1dac8
12334zq <59105356+12334zq@users.noreply.github.com>
2020-06-15 10:27:36 +0800

    Add files via upload

--- IC_GN2.m
+++ IC_GN2.m
@@ -80,12 +80,45 @@ Wdp=[ 0.5*dp(4),dp(5),0.5*dp(6),1+dp(2),dp(3),dp(1);
     0.5*dp(10),dp(11),0.5*dp(12),dp(8),dp(9)+1,dp(7);
     zeros(1,5),1];
 %Wp=warp_fun*pinv(Wdp);
-p=p-dp';
-%p=[Wp(1,3),Wp(1,1)-1,Wp(1,2),Wp(2,3),Wp(2,1),Wp(2,2)-1]
+%p=p-dp';
+Wp=t_warpfun(p)*inv(t_warpfun(dp));
+p=[Wp(4,6),Wp(4,4)-1,Wp(4,5),2*Wp(4,1),Wp(4,2),2*Wp(4,3),Wp(5,6),Wp(5,4),Wp(5,5)-1,2*Wp(5,1),Wp(5,2),2*Wp(5,3)];
 p1=[p1;p];
 %CNSSD=sum(sum((refgray+F1*dp-refmean)/dref-(curgray-curmean)/dcur).^2);
 ZNSSD=sum(((refgray-refmean)./dref-(curgray-curmean)./dcur).^2);
 if (dp(1)^2+dp(7)^2)<crition
     break
 end
+end
+end
+function S=t_warpfun(p)
+    %High-efficiency and high-accuracy digital image correlation for three-dimensional measurement
+    %p=[u,ux,uy,uxx,uxy,uyy,v,vx,vy,vxx,vxy,vyy]
+    %p=[1, 2, 3, 4,  5,   6,7, 8, 9, 10, 11,12]
+    
+    S(1,1)=1+2*p(2)+p(2)^2+p(1)*p(4);
+    S(1,2)=2*p(1)*p(5)+2*(1+p(2))*p(3);
+    S(1,3)=p(3)^2+p(1)*p(6);
+    S(1,4)=2*p(1)*(1+p(2));
+    S(1,5)=2*p(1)*p(3);
+    S(1,6)=p(1)^2;
+
+    S(2,1)=0.5*(p(7)*p(4)+2*(1+p(2))*p(8)+p(1)*p(10));
+    S(2,2)=1+p(3)*p(8)+p(2)*p(9)+p(7)*p(5)+p(1)*p(11)+p(9)+p(2);
+    S(2,3)=0.5*(p(7)*p(6)+2*p(3)*(1+p(9))+p(1)*p(12));
+    S(2,4)=p(7)+p(7)*p(2)+p(1)*p(8);
+    S(2,5)=p(1)+p(7)*p(3)+p(1)*p(9);
+    S(2,6)=p(1)*p(7);
+   %p=[u,ux,uy,uxx,uxy,uyy,v,vx,vy,vxx,vxy,vyy]
+    %p=[1, 2, 3, 4,  5,  6,7, 8, 9, 10, 11,12] 
+    S(3,1)=p(8)^2+p(7)*p(10);
+    S(3,2)=2*p(7)*p(11)+2*p(8)*(1+p(9));
+    S(3,3)=1+2*p(9)+p(9)^2+p(7)*p(12);
+    S(3,4)=2*p(7)*p(8);
+    S(3,5)=2*p(7)*(1+p(9));
+    S(3,6)=p(7)^2;
+    
+    S(4:6,:)=[ 0.5*p(4),p(5),0.5*p(6),1+p(2),p(3),p(1);
+        0.5*p(10),p(11),0.5*p(12),p(8),p(9)+1,p(7);
+        zeros(1,5),1];
 end
